9e8c5964f67b397112d990dcb2869202e1574c47
Ganesh <gsubu1@gmail.com>
2022-03-17 15:34:48 -0400

    Fixed include paths

--- src/main/cml.c
+++ src/main/cml.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include<math.h>
-#include "lib/argparser/argparser.h"
-#include "learn/static.h"
+#include "../lib/argparser/argparser.h"
+#include "../learn/static.h"
 
 int main(int argc, char** argv) {
    
